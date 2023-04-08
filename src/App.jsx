d6a937af28806ce8aeca58478c47eff7b271da35
Navindu-Abhishek <abi2000navi@gmail.com>
2023-04-08 16:35:23 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 
+// import dotenv from "dotenv";
+// dotenv.config();
 
 function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
-  
-  const API_URL = 'https://api.openweathermap.org/data/2.5';
-  const API_KEY = 'a2767a1d3470df78ba6f059b52aff766';
-
 
   useEffect(() => {
     const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function (position) {
+      navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
 
-      await fetch(`${API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${API_KEY}`)
-
-        .then((res) => res.json())
-        .then((result) => {
-          setData(result);
-          console.log(result);
-        });
-    };
+      await fetch(`${import.meta.env.VITE_API_KEY}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${import.meta.env.VITE_API_URL}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
     fetchData();
-  }, [lat, long]);
+  }, [lat,long])
 
   return (
     <div className="App">
